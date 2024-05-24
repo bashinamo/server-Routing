be73fb24bb5a1c2cb49dad7e854b6664f8081bb8
bashin <goblinsgoldg@gmail.com>
2024-05-24 07:01:40 +0200

    kapa WEB API med REST

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
+
 
 // Import routes
 const userRoutes = require('./routes/users');
